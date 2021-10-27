56c09ff2243f20bd04e3e8985443758e235a3672
BossySmaxx <46630366+BossySmaxx@users.noreply.github.com>
2021-10-27 17:20:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const path = require('path');
+cont cors = require('cors');
 
 const app = express();
 app.use(express.static(path.join(__dirname, './public')));
+app.use(cors());
 
 const server = http.createServer(app);
 const io = socketio(server);
