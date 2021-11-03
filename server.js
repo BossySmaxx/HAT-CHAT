96a793df7f08b1ec02017388cc675a7a739a33cc
BossySmaxx <46630366+BossySmaxx@users.noreply.github.com>
2021-11-03 16:15:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, './public')));
 app.use(cors({origin: "*"}));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const server = http.createServer(app);
 const io = socketio(server);
 
