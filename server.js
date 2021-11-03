62c561026137ec49d81942a93b02b1ffab55fa95
BossySmaxx <46630366+BossySmaxx@users.noreply.github.com>
2021-11-03 16:26:02 +0530

    removed unused lines

--- server.js
+++ server.js
@@ -8,15 +8,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, './public')));
 app.use(cors({origin: "*"}));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 const server = http.createServer(app);
 const io = socketio(server);
 
-server.listen(3000, console.log(":Listenign on PORT /8800"));
+server.listen(3000, console.log(":Listenign on PORT /3000"));
 
 module.exports = io;
